7e19ac4c9994312700003fc52b6a2fa1af04e867
Marcos Bahamondes <mbahamondes.osorio@gmail.com>
2019-03-07 16:33:51 -0300

    commit inicial

---
+++ api/DB.js
@@ -0,0 +1,3 @@
+module.exports = {
+    DB: 'mongodb://user-app-delivery:admin12345@cluster0-shard-00-00-h57ss.mongodb.net:27017,cluster0-shard-00-01-h57ss.mongodb.net:27017,cluster0-shard-00-02-h57ss.mongodb.net:27017/ng7crud?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true'
+};
